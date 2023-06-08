ed22899657745296575a4edd94d3c1a72f71016c
Limbo <1034935762@qq.com>
2023-06-08 20:53:08 +0800

    2023年6月8日20:53:06

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,48 @@
 <template>
-	<div class="home">
-		<div @click="push">
-            BBB
-		</div>
-    <!-- <button @click="$toast">to123123123ast</button> -->
-	</div>
+    <div class="home">
+        <div @click="push" class="red">BBB</div>
+        <div>11231223123</div>
+        {{txt}}
+        <!-- <button @click="$toast">to123123123ast</button> -->
+    </div>
 </template>
-
 
 <script>
 // @ is an alias to /src
 
 export default {
-	name: 'Home',
-	components: {},
-	methods: {
-		push() {
-			this.$router.push({
-				name: 'routerParams',
-				params: {
-					a: 1,
-					c: 1
-				}
-			})
-		}
-	}
-}
+    name: 'Home',
+    data(){
+        return {
+            txt:''
+        }
+    },
+    asyncData(){
+        console.log(123,typeof window)
+    },
+    created() {
+        console.log('create')
+        if (typeof window == 'undefined') {
+            console.log(1)
+            this.txt = 'server';
+        }
+    },
+    methods: {
+        push() {
+            this.$router.push({
+                name: 'routerParams',
+                params: {
+                    a: 1,
+                    c: 1,
+                },
+            });
+        },
+    },
+};
 </script>
+
+<style lang="scss" scoped>
+.red {
+    color: blue;
+}
+</style>
