6ba1890f2664e2fc25f296a90c387f44e83f9d0a
Limbo <1034935762@qq.com>
2023-06-14 20:53:33 +0800

    2023年6月14日20:53:32

--- src/utils/request-server.js
+++ src/utils/request-server.js
@@ -1,30 +1,36 @@
 import axios from "axios/dist/node/axios.cjs"
 
-export default function (apiConfig) {
-	const axiosInstance = axios.create({
-		baseURL: process.env.VUE_APP_BASE_API,
-		timeout: 1000 * 30,
-		headers: {
-			"Content-Type": "application/json; charset=utf-8"
-		}
-	})
-	// 请求拦截器
-	axiosInstance.interceptors.request.use(
-		(config) => {
-			return config
-		},
-		(err) => {
-			return Promise.reject(err)
-		}
-	)
-	// 响应拦截器
-	axiosInstance.interceptors.response.use(
-		(res) => {
-			return res.data
-		},
-		(err) => {
-			return Promise.reject(err)
-		}
-	)
-	return axiosInstance(apiConfig)
+export class AxiosClass {
+    constructor(config) {
+        const axiosInstance = axios.create(config);
+        // 请求拦截器
+        axiosInstance.interceptors.request.use(
+            (config) => {
+                return config;
+            },
+            (err) => {
+                return Promise.reject(err);
+            }
+        );
+        // 响应拦截器
+        axiosInstance.interceptors.response.use(
+            (res) => {
+                return res.data;
+            },
+            (err) => {
+                return Promise.reject(err);
+            }
+        );
+        return axiosInstance;
+    }
 }
+
+const service = new AxiosClass({
+    baseURL: process.env.VUE_APP_BASE_API,
+    timeout: 1000 * 30,
+    headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+    },
+});
+
+export default service;
