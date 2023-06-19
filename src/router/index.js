b47283dc745c2d9906f518dd9fc15c706e639cd2
Limbo <1034935762@qq.com>
2023-06-19 17:31:32 +0800

    2023年6月19日17:31:32

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
 
+import useRoutes from "./modules/user"
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -9,18 +11,7 @@ const routes = [
 		name: "Home",
 		component: () => import("@/views/Home.vue")
 	},
-	{
-		path: "/user",
-		name: "User",
-		meta: { title: "user" },
-		component: () => import("@/views/user/index.vue")
-	},
-	{
-		path: "/userlist",
-		name: "Userlist",
-		meta: { title: "Userlist" },
-		component: () => import("@/views/user/userList.vue")
-	},
+	useRoutes,
 	{
 		path: "/login",
 		name: "Login",
@@ -32,24 +23,22 @@ const routes = [
 		name: "404",
 		component: () => import("@/views/404"),
 		meta: { title: "404" }
-	}
+	},
+	{ path: "*", redirect: "/404" }
 ]
 
 // 跳转相同路由报错
-const originalPush = VueRouter.prototype.push;
+const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location, onResolve, onReject) {
-    if (onResolve || onReject)
-        return originalPush.call(this, location, onResolve, onReject);
-    return originalPush.call(this, location).catch((err) => err);
-};
+	if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
+	return originalPush.call(this, location).catch((err) => err)
+}
 // replace相同路由报错
-const originalReplace = VueRouter.prototype.replace;
+const originalReplace = VueRouter.prototype.replace
 VueRouter.prototype.replace = function replace(location, onResolve, onReject) {
-    if (onResolve || onReject)
-        return originalReplace.call(this, location, onResolve, onReject);
-    return originalReplace.call(this, location).catch((err) => err);
-};
-
+	if (onResolve || onReject) return originalReplace.call(this, location, onResolve, onReject)
+	return originalReplace.call(this, location).catch((err) => err)
+}
 
 export function createRouter() {
 	return new VueRouter({
---
+++ src/router/modules/user.js
@@ -0,0 +1,20 @@
+import PureRouterView from "@/layouts/pureRouterView.vue"
+
+export default {
+	path: "/usernest",
+	component: PureRouterView,
+	children: [
+		{
+			path: "/user",
+			name: "User",
+			meta: { title: "user" },
+			component: () => import("@/views/user/index.vue")
+		},
+		{
+			path: "/userlist",
+			name: "Userlist",
+			meta: { title: "Userlist" },
+			component: () => import("@/views/user/userList.vue")
+		}
+	]
+}
