bfbf21447b41b270795f97960c9c5958b254d5db
Limbo <1034935762@qq.com>
2023-06-20 17:34:21 +0800

    2023年6月20日17:34:20

--- src/utils/request-server.js
+++ src/utils/request-server.js
@@ -1,38 +1,46 @@
 import axios from "axios/dist/node/axios.cjs"
 const successCode = [200, "200"]
 
-export default function (apiConfig) {
-	const axiosInstance = axios.create({
-		baseURL: process.env.VUE_APP_BASE_API,
-		timeout: 1000 * 30,
-		withCredentials: true,
-		headers: {
-			"Content-Type": "application/json; charset=utf-8"
-		}
-	})
-	// 请求拦截器
-	axiosInstance.interceptors.request.use(
-		(config) => {
-			return config
-		},
-		(err) => {
-			return Promise.reject(err)
-		}
-	)
-	// 响应拦截器
-	axiosInstance.interceptors.response.use(
-		(res) => {
-			const code = res.data.code
-			if (successCode.includes(code)) {
-				return res.data
-			} else {
-				// res.data:{code: '500', msg: 'xxx'}
-				return Promise.reject(res.data)
+export class AxiosClass {
+	constructor(config) {
+		const axiosInstance = axios.create(config)
+		// 请求拦截器
+		axiosInstance.interceptors.request.use(
+			(config) => {
+				return config
+			},
+			(err) => {
+				return Promise.reject(err)
 			}
-		},
-		(err) => {
-			return Promise.reject(err)
-		}
-	)
-	return axiosInstance(apiConfig)
+		)
+		// 响应拦截器
+		axiosInstance.interceptors.response.use(
+			(res) => {
+				const code = res.data.code
+				if (successCode.includes(code)) {
+					return res.data
+				} else {
+					// res.data:{code: '500', msg: 'xxx'}
+					return Promise.reject(res.data)
+				}
+			},
+			(err) => {
+				return Promise.reject(err)
+			}
+		)
+		return axiosInstance
+	}
+}
+
+const service = new AxiosClass({
+	baseURL: process.env.VUE_APP_BASE_API,
+	timeout: 1000 * 30,
+	withCredentials: true,
+	headers: {
+		"Content-Type": "application/json; charset=utf-8"
+	}
+})
+
+export default function (apiConfig) {
+	return service(apiConfig)
 }
